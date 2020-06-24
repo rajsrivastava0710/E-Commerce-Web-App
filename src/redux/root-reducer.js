6a68c6822bbb266b9adf75bd3600d985c4ac850a
Raj <rajsriv.14@gmail.com>
2020-06-25 00:33:23 +0530

    Added persistance for cart items

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,24 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root', //where do we want to start storing
+    storage, //storage
+    whitelist: ['cart'] //what reducer u want to persist
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
-})
+}) 
+
+export default persistReducer(persistConfig, rootReducer);
+
+// export default combineReducers({
+//     user: userReducer,
+//     cart: cartReducer
+// })
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 const middlewares = [logger]; //in future we may need more than 1 , so array
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store);
+
+export { store , persistor };
